bb0cc9d75f77f40206cb60a26c2395384564647d
CWCampbell <CWCampbell@users.noreply.github.com>
2015-06-21 17:23:19 -0400

    improved comments

--- asst2-final.R
+++ asst2-final.R
@@ -4,19 +4,28 @@
 #x<-matrix(1:4,2,2) #data
 #z<-matrix(3:6,2,2) #data
 
-makeCacheMatrix <- function(x = matrix()){ #function takes matrix as input
-        m <- NULL #setting inverse variable to null
-        #assign("m", NULL, envir= .GlobalEnv) 
-        set <- function(y) { #naming and assigning subfunction; 
-                x <<- y #turns new input into live input
-                m <<- NULL #resets m to null, one level up (parent environment)
+#Function takes matrix as input, setting inverse variable to null, assigns m to null in global environment; 
+#then names and assigns subfunction. After turning new input into live input, it resets m to null in the parent environment.
+
+makeCacheMatrix <- function(x = matrix()){ 
+        m <- NULL 
+        set <- function(y) { 
+                x <<- y 
+                m <<- NULL 
         } 
-        get <- function() x # naming and assigning subfunction: retrieves current data matrix
-        setMatrix <- function(x) {  # naming and assigning subfunction: takes current data matrix and calculates its inverse
-                m <-solve(x) # assigns name m to inverse
+
+#Name and assign subfunction to retrieve current data matrix; name and assign subfunction to take current data matrix 
+#and calculates its inverse. The value is assigned to the variable m.
+
+        get <- function() x 
+        setMatrix <- function(x) {  
+                m <-solve(x) 
                 } 
-        getMatrix <- function() m  #naming and assigning subfunction: retrieves inverse
-        list(set=set, get=get, setMatrix=setMatrix, #list items (names of subfunctions) for function 
+
+#Name and assign subfunction to retrieve inverse; must list items (names of subfunctions) for function 
+
+        getMatrix <- function() m  
+        list(set=set, get=get, setMatrix=setMatrix, 
              getMatrix=getMatrix)
        
 }
@@ -28,14 +37,20 @@ makeCacheMatrix <- function(x = matrix()){ #function takes matrix as input
 # ## If the inverse has already been calculated (and the matrix has not changed), 
 # ## then the cachesolve should retrieve the inverse from the cache.
 
-cacheSolve <- function(x=matrix(),...){ #function takes matrix as input
-                        if(!is.null(m)){ #check to see if existing m is null; if not...
-                        message("getting cached data") #throw message saying "getting cached data"
-                        print(m) # print cached inverse
+
+
+
+#function takes matrix as input; first check to see if existing m is null; if not, throw message 
+#saying "getting cached data" and print cached inverse. If existing m is indeed null, solve the matrix 
+#and store the resulting inverse as a variable named m. Then print new inverse. 
+
+cacheSolve <- function(x=matrix(),...){ 
+                        if(!is.null(m)){ 
+                        message("getting cached data") 
+                        print(m) 
                         }
-                        else{ #if existing m is indeed null...
-                        m<<-solve(x)    #solve the matrix and put the resulting inverse in a variable named m    
-                        print(m)       #print new inverse 
+                        else{ 
+                        m<<-solve(x)    
+                        print(m)       
                         }
       }
-
